8659c542e11933d88d190fe7e76d77dfdae3504a
GeneralB5 <ianmarcobonfiglio@gmail.com>
2024-06-06 14:43:04 -0300

    First Commit

---
+++ app.js
@@ -0,0 +1,31 @@
+import express, { json, urlencoded } from "express"
+
+const app = express()
+app.use(json({limit:'50mb'}))
+app.use(urlencoded({limit:'50mb',extended:true}))
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+export default app
